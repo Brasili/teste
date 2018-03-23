0c15e662ef4c218bab3f6823a7c4b82b4bfb38e1
Brasili <matheusbrasili.ifsp@gmail.com>
2018-03-23 17:12:41 -0300

    v4 escritorio

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 	echo"Primeira versão do index.php - salvo no escritório";
 	
 	echo"Alteração realizada em casa";
-
-
+	
+	
 	echo"Alteração realizada no escritório"
-
+	
 	echo"Alteração realizada em casa novamente";
-
+	
 	
 ?>
